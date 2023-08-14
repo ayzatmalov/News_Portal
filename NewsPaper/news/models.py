0f48d54cfac66095cc690fc96414914991a6ddda
ayzatmalov <ayzat.malov@gmail.com>
2023-08-15 01:40:28 +0300

    Creation three methods accordance with reference

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 class Author(models.Model):
-    pass
+
+    def update_rating(self):
+        pass
 
 
 class Category(models.Model):
@@ -9,12 +11,22 @@ class Category(models.Model):
 
 
 class Post(models.Model):
-    pass
+
+    def like(self):
+        pass
+
+    def dislike(self):
+        pass
 
 
 class PostCategory(models.Model):
     pass
 
 
-class Category(models.Model):
-    pass
+class Comment(models.Model):
+
+    def like(self):
+        pass
+
+    def dislike(self):
+        pass
