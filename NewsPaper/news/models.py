d5de9b645c54b4ab1dd94233844bd2b74084c6aa
ayzatmalov <ayzat.malov@gmail.com>
2023-08-15 21:59:48 +0300

    Creation necessary fields accordance with techtask

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -1,16 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Author(models.Model):
+    authorUser = models.OneToOneField(User, on_delete=models.CASCADE)
+    ratingAuthor = models.SmallIntegerField(default=0)
+
 
     def update_rating(self):
         pass
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=64, unique=True)
 
 
 class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    NEWS = 'NW'
+    ARTICLE = 'AR'
+    CATEGORY_CHOICES = (
+        (NEWS, 'Новость'),
+        (ARTICLE, 'Статья'),
+    )
+    categoryType = models.CharField(max_length=2, choices=CATEGORY_CHOICES, default=ARTICLE)
+    dateCreation = models.DateTimeField(auto_now_add=True)
+    postCategory = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=128)
+    text = models.TextField()
+    rating = models.SmallIntegerField(default=0)
 
     def like(self):
         pass
@@ -18,12 +36,21 @@ class Post(models.Model):
     def dislike(self):
         pass
 
+    def preview(self):
+        return None
+
 
 class PostCategory(models.Model):
-    pass
+    postThrough = models.ForeignKey(Post, on_delete=models.CASCADE)
+    categoryThrough = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
 class Comment(models.Model):
+    commentPost = models.ForeignKey(Post, on_delete=models.CASCADE)
+    commentUser = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    dateCreation = models.DateTimeField(auto_now_add=True)
+    rating = models.SmallIntegerField(default=0)
 
     def like(self):
         pass
