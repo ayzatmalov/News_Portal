01038822f3125dfc6eb8aa5f900e5c95fd923604
ayzatmalov <ayzat.malov@gmail.com>
2023-08-24 22:08:28 +0300

    Make the page /news/ wiht list of posts + each post can be show on separate page (/news/id) + all pages based on the template default.html

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView # Import class, for show list of objects from DB
+from .models import Post
+from datetime import datetime
 
 # Create your views here.
+class PostList(ListView):
+    model = Post # indicate the model, whose objects we will show
+    ordering = 'title'
+    template_name = 'news.html'
+    context_object_name = 'news'
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
